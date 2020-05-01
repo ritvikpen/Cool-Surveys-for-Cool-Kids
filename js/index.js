29d958352fc75e11d6e1e66341dda445f9f1e371
ritvikpen <ritviksiddha@gmail.com>
2020-05-01 12:48:02 -0700

    Done

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// Get a reference to the database service
+var database = firebase.database();
+
+function writeUserData(name, age, gender, marital, employment, health, data) {
+    firebase.database().ref(name).set({
+      age: age,
+      gender: gender,
+      marital_status: marital,
+      employment_statues: employment,
+      health_conditions: health,
+      survey_data: data
+    });
+  }
+
+function submit_dat(){
+  writeUserData(
+    $("#user_name").val(), 
+    $("#age").val(),
+    $("#gender").val(),
+    $("#married").val(),
+    $("#job").val(),
+    $("#health").val(),
+    $("#survey_data").val()
+  );
+}
